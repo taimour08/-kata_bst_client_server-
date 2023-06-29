0d2829241800c13274d5c812abaedcfeca900733
root <root@DESKTOP-BQ53NFE>
2023-06-29 13:33:11 +0300

    Send number from client, diplay on server

--- client.cpp
+++ client.cpp
@@ -4,51 +4,41 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-// Function to send commands to the server and receive responses
-void sendCommand(int serverSocket, const std::string& command) {
-    // TODO: Implement command sending logic
-}
-
-// Client program entry point
 int main() {
     // Create socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
-        std::cerr << "Failed to create socket\n";
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "Failed to create socket." << std::endl;
         return 1;
     }
 
-    // Set up server address and port
+    // Connect to server
     sockaddr_in serverAddress{};
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(12345); // Replace with the server's port number
+    serverAddress.sin_port = htons(12345);  // Use the same port number as the server
     if (inet_pton(AF_INET, "127.0.0.1", &(serverAddress.sin_addr)) <= 0) {
-        std::cerr << "Invalid address or address not supported\n";
-        close(serverSocket);
+        std::cerr << "Failed to set server address." << std::endl;
         return 1;
     }
 
-    // Connect to the server
-    if (connect(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        std::cerr << "Failed to connect to server\n";
-        close(serverSocket);
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Failed to connect to the server." << std::endl;
         return 1;
     }
 
-    // Send commands to the server
-    std::string command;
-    while (true) {
-        std::cout << "Enter command: ";
-        std::getline(std::cin, command);
+    // Input a number
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
 
-        // Send the command to the server
-        sendCommand(serverSocket, command);
-
-        // TODO: Receive and print the server's response
-
-        // TODO: Handle termination condition (e.g., if the user enters an exit command)
+    // Send number to server
+    if (send(clientSocket, &number, sizeof(number), 0) == -1) {
+        std::cerr << "Failed to send number to server." << std::endl;
+        return 1;
     }
 
-    close(serverSocket);
+    // Close socket
+    close(clientSocket);
+
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,77 +1,56 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 
-// Function to handle client commands
-void handleCommand(int clientSocket, const std::string& command) {
-    // TODO: Implement command handling logic
-}
-
-// Server program entry point
 int main() {
     // Create socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
-        std::cerr << "Failed to create socket\n";
+        std::cerr << "Failed to create socket." << std::endl;
         return 1;
     }
 
-    // Bind socket to a specific IP and port
+    // Bind socket to a specific port
     sockaddr_in serverAddress{};
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(12345); // Replace with your desired port number
+    serverAddress.sin_port = htons(12345);  // Choose any port number
 
-    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        std::cerr << "Failed to bind socket\n";
-        close(serverSocket);
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Failed to bind socket to port." << std::endl;
         return 1;
     }
 
     // Listen for incoming connections
-    if (listen(serverSocket, 5) == -1) {
-        std::cerr << "Failed to listen for connections\n";
-        close(serverSocket);
+    if (listen(serverSocket, 1) == -1) {
+        std::cerr << "Failed to listen for connections." << std::endl;
         return 1;
     }
 
-    // Accept client connections and handle commands
-    while (true) {
-        sockaddr_in clientAddress{};
-        socklen_t clientAddressLength = sizeof(clientAddress);
-        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
-        if (clientSocket == -1) {
-            std::cerr << "Failed to accept client connection\n";
-            continue;
-        }
+    std::cout << "Server is listening for connections..." << std::endl;
 
-        char clientIP[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(clientAddress.sin_addr), clientIP, INET_ADDRSTRLEN);
-        std::cout << "Accepted connection from client: " << clientIP << std::endl;
-
-        // Receive commands from the client and handle them
-        char buffer[1024];
-        ssize_t bytesRead;
-        std::string command;
-
-        while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
-            command.append(buffer, bytesRead);
-            if (command.find('\n') != std::string::npos) {
-                // Process the complete command
-                handleCommand(clientSocket, command);
-                command.clear();
-            }
-        }
-
-        if (bytesRead == -1) {
-            std::cerr << "Failed to receive data from client\n";
-        }
-
-        close(clientSocket);
+    // Accept a client connection
+    sockaddr_in clientAddress{};
+    socklen_t clientAddressLength = sizeof(clientAddress);
+    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+    if (clientSocket == -1) {
+        std::cerr << "Failed to accept client connection." << std::endl;
+        return 1;
     }
 
+    // Receive number from client
+    int number;
+    if (recv(clientSocket, &number, sizeof(number), 0) == -1) {
+        std::cerr << "Failed to receive number from client." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Received number from client: " << number << std::endl;
+
+    // Close sockets
+    close(clientSocket);
     close(serverSocket);
+
     return 0;
 }
