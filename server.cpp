d2ee40d99043e863f70a4aa3def330976f41c85d
root <root@DESKTOP-BQ53NFE>
2023-06-30 16:32:05 +0300

    Edit main

--- server.cpp
+++ server.cpp
@@ -195,53 +195,8 @@ private:
 };
 
 int main() {
-    // Create socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
-        std::cerr << "Failed to create socket." << std::endl;
-        return 1;
-    }
-
-    // Bind socket to a specific port
-    sockaddr_in serverAddress{};
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(12345);  // Choose any port number
-
-    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
-        std::cerr << "Failed to bind socket to port." << std::endl;
-        return 1;
-    }
-
-    // Listen for incoming connections
-    if (listen(serverSocket, 1) == -1) {
-        std::cerr << "Failed to listen for connections." << std::endl;
-        return 1;
-    }
-
-    std::cout << "Server is listening for connections..." << std::endl;
-
-    // Accept a client connection
-    sockaddr_in clientAddress{};
-    socklen_t clientAddressLength = sizeof(clientAddress);
-    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
-    if (clientSocket == -1) {
-        std::cerr << "Failed to accept client connection." << std::endl;
-        return 1;
-    }
-
-    // Receive number from client
-    int number;
-    if (recv(clientSocket, &number, sizeof(number), 0) == -1) {
-        std::cerr << "Failed to receive number from client." << std::endl;
-        return 1;
-    }
-
-    std::cout << "Received number from client: " << number << std::endl;
-
-    // Close sockets
-    close(clientSocket);
-    close(serverSocket);
+    BSTServer server;
+    server.run(12345); // Start the server
 
     return 0;
 }
