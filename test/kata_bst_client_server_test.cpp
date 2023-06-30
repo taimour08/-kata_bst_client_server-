f9545d0332a587562efbcdbc72c2b302c32cc71a
root <root@DESKTOP-BQ53NFE>
2023-06-30 15:13:07 +0300

    Test for error message on deletion of non existing insertion

--- test/kata_bst_client_server_test.cpp
+++ test/kata_bst_client_server_test.cpp
@@ -26,5 +26,11 @@ EXPECT_EQ(serverResponse, "not found"); // Test the server's response
 EXPECT_EQ("insert 5", ""); // Test the command sent by the client
 EXPECT_EQ(serverResponse, "ERROR: 5 already exists in BST"); // Test the server's response
 
+// Client command: delete 7
+// Server response: ERROR: 7 not found in BST
+EXPECT_EQ("delete 7", ""); // Test the command sent by the client
+EXPECT_EQ(serverResponse, "ERROR: 7 not found in BST"); // Test the server's response
+
+
 
 
